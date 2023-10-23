00f6a39c6037d5f96a48e7cbb60b22e45d8ff6a6
NguedjuiTresor <tresorkouakou@gmail.com>
2023-10-23 16:28:30 +0200

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import ButtonAppBar from "@/components/ui/Header";
+
 const Home = () => {
   return (
+    <div>
+    <ButtonAppBar />
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    </div>
   );
 };
 
